0157bcae86a0ec962ca376fe72667fde200bd898
John Villafania <johnarnoldvillafania@gmail.com>
2023-05-04 19:40:00 +1200

    Add files via upload

---
+++ _nuxt/index.884ea844.js
@@ -0,0 +1,1 @@
+import{a as e,b as o,o as s,e as t}from"./entry.c6d3b4d1.js";const c={},n=t("h1",null,"Crash",-1),a=[n];function r(_,d){return s(),o("div",null,a)}const i=e(c,[["render",r]]);export{i as default};
