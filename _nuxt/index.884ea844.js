e51c52db4528547ee1d0443195b1653f72aadf15
John Villafania <johnarnoldvillafania@gmail.com>
2023-06-09 09:18:18 +0000

    redirection

--- _nuxt/index.884ea844.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as o,o as s,e as t}from"./entry.c6d3b4d1.js";const c={},n=t("h1",null,"Crash",-1),a=[n];function r(_,d){return s(),o("div",null,a)}const i=e(c,[["render",r]]);export{i as default};
